93795a5f0ec82f12790502cb917c6088c77eed7a
Anu <75754430+anupanday@users.noreply.github.com>
2021-04-29 17:46:38 +0100

    Add html, js, css and images files

---
+++ response.js
@@ -0,0 +1,24 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+  var submitbutton = document.getElementById("home-button");
+  submitbutton.addEventListener("click", submit);
+
+  
+
+});
+document.addEventListener('DOMContentLoaded', function() {
+var contactbutton = document.getElementById("contact-button");
+  contactbutton.addEventListener("click", contact);
+});
+function submit (){
+    alert ("Thank you for signing up with us!")
+};
+
+function contact (){
+    alert("Your enquiry has been submitted");
+};
+
+
+
+
+
